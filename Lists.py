4318699f1adb10ba814e88739e5fefee71dc0087
Adarsh Kumar Singh <95336274+034adarsh@users.noreply.github.com>
2023-01-11 22:31:59 +0530

    Create Lists.py

---
+++ Lists.py
@@ -0,0 +1,19 @@
+if __name__ == '__main__':
+    N = int(input())
+    list =[]
+    for i in range(N):
+        string = input().split()
+        if string[0] == 'insert' :
+            list.insert(int(string[1]),int(string[2]))
+        elif string[0] == 'print':
+            print(list)
+        elif string[0] =='remove':
+            list.remove(int(string[1]))
+        elif string[0]=='append':
+            list.append(int(string[1]))
+        elif string[0]=='pop':
+            list.pop()
+        elif string[0]=='sort':
+            list.sort()
+        elif string[0]=='reverse':
+            list.reverse()
